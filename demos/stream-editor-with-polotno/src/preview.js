a369133e2682dfa073b6de018d8a4ad9388f2f07
Anton Lavrenov <lavrton@gmail.com>
2021-04-26 22:37:16 -0500

    fix canvas size

--- demos/stream-editor-with-polotno/src/preview.js
+++ demos/stream-editor-with-polotno/src/preview.js
@@ -1,8 +1,8 @@
-import React from "react";
-import Konva from "konva";
-import { SceneCanvas } from "konva/lib/Canvas";
-import { Navbar, Button } from "@blueprintjs/core";
-import { observer } from "mobx-react-lite";
+import React from 'react';
+import Konva from 'konva';
+import { SceneCanvas } from 'konva/lib/Canvas';
+import { Navbar, Button } from '@blueprintjs/core';
+import { observer } from 'mobx-react-lite';
 
 export const Preview = observer(({ store }) => {
   const [previewVisible, setPreviewVisible] = React.useState(true);
@@ -13,12 +13,12 @@ export const Preview = observer(({ store }) => {
     const canvas = new SceneCanvas({
       width: store.width,
       height: store.height,
-      pixelRatio: 1
+      pixelRatio: 1,
     });
-    canvasRef.current = canvas._canvas;
+    canvasRef.current = canvas;
 
-    const page = Konva.stages[0].findOne(".elements-container");
-    document.querySelector(".preview-container").appendChild(canvas._canvas);
+    const page = Konva.stages[0].findOne('.elements-container');
+    document.querySelector('.preview-container').appendChild(canvas._canvas);
     setInterval(() => {
       canvas.getContext().clear();
       page.drawScene(canvas, page);
@@ -26,26 +26,27 @@ export const Preview = observer(({ store }) => {
   }, []);
 
   React.useEffect(() => {
-    Object.assign(canvasRef.current.style, {
-      position: "",
-      width: store.width / 5 + "px",
-      height: store.height / 5 + "px"
+    canvasRef.current.setSize(store.width, store.height);
+    Object.assign(canvasRef.current._canvas.style, {
+      position: '',
+      width: store.width / 5 + 'px',
+      height: store.height / 5 + 'px',
     });
   }, [store.width, store.height]);
 
   return (
     <div
       style={{
-        position: "fixed",
-        bottom: "20px",
+        position: 'fixed',
+        bottom: '20px',
         right: 0,
         zIndex: 10,
         zIndex: 10000,
-        transformOrigin: "top left",
-        background: "white",
-        border: "1px solid rgba(16, 22, 26, 0.2)",
-        borderRadius: "5px",
-        overflow: "hidden"
+        transformOrigin: 'top left',
+        background: 'white',
+        border: '1px solid rgba(16, 22, 26, 0.2)',
+        borderRadius: '5px',
+        overflow: 'hidden',
       }}
     >
       <Navbar>
@@ -74,10 +75,7 @@ export const Preview = observer(({ store }) => {
           )}
         </Navbar.Group>
       </Navbar>
-      <div
-        className="preview-container"
-        style={{ display: previewVisible ? "" : "none" }}
-      />
+      <div className="preview-container" style={{ display: previewVisible ? '' : 'none' }} />
     </div>
   );
 });
